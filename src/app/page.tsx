0d389d9d43971778b171bf3c6fa9216f6d19488a
unknown <ramsha.tahir23@gmail.com>
2025-01-31 12:46:50 -0800

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,32 @@
+import Apps from "@/Components/Apps";
+import Customize from "@/Components/Customize";
+import Extension from "@/Components/Extension";
+import Footer from "@/Components/Footer";
+import Freetrial from "@/Components/Freetrial";
+import Hero from "@/Components/Hero";
+import Pricing from "@/Components/Pricing";
+import Sponsors from "@/Components/Sponsors";
+import Testimonial from "@/Components/Testimonial";
+import Workpage from "@/Components/Workpage";
+import Yourdata from "@/Components/Yourdata";
+import Yourwork from "@/Components/Yourwork";
+
+
+export default function Home() {
+  return (
+    <div>
+      <Hero />
+      <Workpage />
+      <Extension />
+      <Customize />
+      <Pricing />
+      <Yourwork />
+      <Yourdata />
+      <Sponsors />
+      <Apps />
+      <Testimonial />
+      <Freetrial />
+      <Footer />
+    </div>
+  )
+}
